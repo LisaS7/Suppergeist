a56927275767f3f27d8fa3b4e7034d73151d4465
Lisa <mclisa00@gmail.com>
2026-04-19 16:00:00 +0100

    I thought this was dead code...turns out it isn't. Oops.

--- src/main/java/com/example/suppergeist/repository/MealIngredientRepository.java
+++ src/main/java/com/example/suppergeist/repository/MealIngredientRepository.java
@@ -94,4 +94,45 @@ public class MealIngredientRepository {
             }
         }
     }
+
+    public List<MealIngredientRow> getIngredientsWithNutritionForMeal(int mealId) throws SQLException {
+        String sql = """
+                SELECT mi.ingredient_id, i.name, mi.quantity, mi.unit, i.food_code,
+                       i.energy_kcal, i.protein_g, i.fat_g, i.carbohydrate_g,
+                       i.total_sugars_g, i.fibre_g, i.vitamin_a_µg, i.vitamin_c_mg,
+                       i.vitamin_d_µg, i.vitamin_e_mg, i.vitamin_b12_µg, i.folate_µg
+                FROM meal_ingredients mi
+                JOIN ingredients i ON mi.ingredient_id = i.id
+                WHERE mi.meal_id = ?
+                ORDER BY i.name
+                """;
+        try (Connection conn = dbManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, mealId);
+            try (ResultSet rs = stmt.executeQuery()) {
+                List<MealIngredientRow> results = new ArrayList<>();
+                while (rs.next()) {
+                    Ingredient ingredient = new Ingredient(
+                            rs.getInt("ingredient_id"),
+                            rs.getString("name"),
+                            rs.getString("food_code"),
+                            nullableDouble(rs, "energy_kcal"),
+                            nullableDouble(rs, "protein_g"),
+                            nullableDouble(rs, "fat_g"),
+                            nullableDouble(rs, "carbohydrate_g"),
+                            nullableDouble(rs, "total_sugars_g"),
+                            nullableDouble(rs, "fibre_g"),
+                            nullableDouble(rs, "vitamin_a_µg"),
+                            nullableDouble(rs, "vitamin_c_mg"),
+                            nullableDouble(rs, "vitamin_d_µg"),
+                            nullableDouble(rs, "vitamin_e_mg"),
+                            nullableDouble(rs, "vitamin_b12_µg"),
+                            nullableDouble(rs, "folate_µg")
+                    );
+                    results.add(new MealIngredientRow(ingredient, rs.getDouble("quantity"), rs.getString("unit")));
+                }
+                return results;
+            }
+        }
+    }
 }
